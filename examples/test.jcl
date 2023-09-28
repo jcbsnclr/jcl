3c7c032c6af284a645297b4c2c7f1f7eb00400b8
Jacob Sinclair <jcbsnclr@outlook.com>
2023-09-29 00:27:09 +0100

    Implement basic multi-char `Token` types Implemented comments, whitespace, and identifiers

--- examples/test.jcl
+++ examples/test.jcl
@@ -1,9 +1,35 @@
-# print "Hello, World!" to console
+# print "Hello, World!" to console. commands are delimited by semicolons.
+
+			# some whitespace
+
 echo "Hello, World!";
 
 # prompt user for their age and parse it as an integer
+#
+# the text between `[ ... ]` is known as an "inline" batch; it evaluates
+# to the result of the commands inside
+# 
+# the syntax of `identifier: value` describes a "pair", which is simply 
+# a key-value pair passed into a command. 
+# 
+# here we decalare a variable called age and give it a value which is the result
+# of a command. the let command is used to define variables
+#
+# the pipe operator is used to take the return value of the previous command and
+# bind it to `$`, and then execute the following command. this is to provide better ergonomics
+# for chaining function calls than e.g. `baz [bar [foo "123"]]`, enabling you to do 
+# `foo "123" | bar $ | baz $`. 
+# (though perhaps in this instance it doesn't aid ergonomics too much :^))
 let age: [prompt "What is your age?" | parse_int $];
 
+# just like let, `if` makes use of pairs. `if` is not special, it is like any other command in
+# the language, which is why we re-use the pair syntax here, to allow us to name a given part 
+# of the command (in this case, the successful and unsuccessful branches).
+#
+# the text between `{ ... }` is called a "batch", and is a list of commands.
+# it is not demonstrated in this instance, but the result of a batch, inline or not, is the 
+# result of the last command in the batch. if the last command in a batch ends in a semicolon,
+# then an empty command is inserted at the end of the batch, and nothing is returned
 if [lt age 18] then: {
 	echo "You are too young to buy alcohol";
 } else: {
--- src/lexer.rs
+++ src/lexer.rs
@@ -1,11 +1,33 @@
 use std::iter::Peekable;
 use std::str::CharIndices;
 
-/// A [Span] represents a slice of a source string
+/// A [`Span`] represents a slice of a source string
 #[derive(Debug, Clone, Copy)]
 pub struct Span {
     start: usize,
     end: usize,
+}
+
+impl Span {
+    /// Create a new [`Span`] over 2 positions
+    fn new(start: usize, end: usize) -> Span {
+        Span { start, end }
+    }
+
+    /// Create a new [`Span`] over one byte
+    fn starting_at(start: usize) -> Span {
+        Span::new(start, start + 1)
+    }
+
+    /// Extend the [`Span`] by the UTF-8 encoded length of a given `char`
+    fn extend_by(&mut self, c: char) {
+        self.end = self.end + c.len_utf8();
+    }
+
+    /// Get a [`std::ops::Range`] representing the [`Span`]
+    pub fn as_range(self) -> std::ops::Range<usize> {
+        self.start..self.end
+    }
 }
 
 /// The different kinds of brackets
@@ -16,7 +38,7 @@ pub enum BracketKind {
     Brace,
 }
 
-/// The different types of [Token] found in the language
+/// The different types of [`Token`] found in the language
 #[derive(Debug, Clone, Copy)]
 pub enum TokenKind {
     Comment,
@@ -36,10 +58,10 @@ pub enum TokenKind {
     Other(char),
 }
 
-/// A [Token] describes a piece of a source string
+/// A [`Token`] describes a piece of a source string
 #[derive(Debug, Clone, Copy)]
 pub struct Token<'a> {
-    /// The [Span] the token occupies
+    /// The [`Span`] the token occupies
     span: Span,
     /// What the token represents
     kind: TokenKind,
@@ -47,19 +69,51 @@ pub struct Token<'a> {
     view: &'a str,
 }
 
-/// The [Lexer] is an iterator over [Token]s within a source string
+/// The [`Lexer`] is an iterator over [`Token`]s within a source string
 pub struct Lexer<'a> {
     source: &'a str,
     stream: Peekable<CharIndices<'a>>,
 }
 
 impl<'a> Lexer<'a> {
-    /// Produce a [Lexer] from a given source string
+    /// Produce a [`Lexer`] from a given source string
     pub fn from_source(source: &'a str) -> Lexer<'a> {
         Lexer {
             source,
             stream: source.char_indices().peekable(),
         }
+    }
+
+    /// Extend a [`Span`] while the stream matches a predicate
+    fn take_while(&mut self, span: &mut Span, pred: impl Fn(char) -> bool) {
+        // consume stream while char matches predicate
+        while let Some((_, c)) = self.stream.next_if(|&(_, c)| pred(c)) {
+            span.extend_by(c);
+        }
+    }
+
+    /// Produce a [`Token`] representing a given [`Span`] of text, with a given [`TokenKind`]
+    ///
+    /// # Panics
+    /// This function will panic if the end of the [`Span`] extends past the end of the
+    /// source string.
+    fn produce_token(&self, kind: TokenKind, span: Span) -> Token<'a> {
+        Token {
+            span,
+            kind,
+            view: &self.source[span.as_range()],
+        }
+    }
+
+    /// Produce a [`Token`] of a given [`TokenKind`] while the stream matches a predicate.
+    /// It returns `None` if the first char it consumes does not match the predicate.
+    fn produce_while(&mut self, kind: TokenKind, pred: impl Fn(char) -> bool) -> Option<Token<'a>> {
+        let &(start, _) = self.stream.peek()?;
+        let mut span = Span::starting_at(start);
+
+        self.take_while(&mut span, pred);
+
+        Some(self.produce_token(kind, span))
     }
 }
 
@@ -67,6 +121,33 @@ impl<'a> Iterator for Lexer<'a> {
     type Item = Token<'a>;
 
     fn next(&mut self) -> Option<Token<'a>> {
-        unimplemented!()
+        let &(_, c) = self.stream.peek()?;
+
+        match c {
+            c if c.is_whitespace() => {
+                self.produce_while(TokenKind::Whitespace, char::is_whitespace)
+            }
+
+            c if rules::identifier_start(c) => {
+                self.produce_while(TokenKind::Identifier, rules::identifier_body)
+            }
+
+            '#' => self.produce_while(TokenKind::Comment, |c| c != '\n'),
+
+            c => unimplemented!("{c}"),
+        }
+    }
+}
+
+// rules for the lexer
+mod rules {
+    /// The start of an identifier. Can be any valid Unicode alphabetic character, or an underscore
+    pub fn identifier_start(c: char) -> bool {
+        c.is_alphabetic() || c == '_'
+    }
+
+    /// The body of an identifier. Can be any valid [`identifier_start`], or any valid Unicode numeric character
+    pub fn identifier_body(c: char) -> bool {
+        identifier_start(c) || c.is_numeric()
     }
 }
